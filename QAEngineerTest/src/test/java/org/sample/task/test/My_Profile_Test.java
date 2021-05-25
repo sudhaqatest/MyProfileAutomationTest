071c13dd1267dae775261294d015751562493a65
sudhaqatest <84685487+sudhaqatest@users.noreply.github.com>
2021-05-25 18:06:19 -0400

    Update My_Profile_Test.java

--- QAEngineerTest/src/test/java/org/sample/task/test/My_Profile_Test.java
+++ QAEngineerTest/src/test/java/org/sample/task/test/My_Profile_Test.java
@@ -31,48 +31,46 @@ public class My_Profile_Test extends TestBase {
 	@BeforeTest
 	public void setup() throws IOException {
 		intialization();
-
 	}
 
 	@Test
-
 	public void Profile_test() throws Exception {
 		LoginPage login = new LoginPage();
 		login.loginToWordPress(p.getProperty("username"), p.getProperty("password"));
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/header/h1")));
-		String Title = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/header/h1")).getText();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("H1")));
+		String Title = driver.findElement(By.tagName("H1")).getText();
 		System.out.println(Title);
 		SoftAssert SoftAssert = new SoftAssert();
 		SoftAssert.assertEquals(Title, "My Profile",
 				"'Verify My Profile Text in Profile Page'-First soft assert failed");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// Enter First Name
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[1]/input"))
-				.sendKeys("");
-		;
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[1]/input"))
-				.sendKeys("Test");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@name,'first_name')]")));
+		driver.findElement(By.xpath("//input[contains(@name,'first_name')]")).clear();
+		driver.findElement(By.xpath("//input[contains(@name,'first_name')]")).sendKeys("Testfirstname");
 		// Enter Last Name
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[2]/input"))
-				.sendKeys("Test");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@id,'last_name')]")));
+		driver.findElement(By.xpath("//input[contains(@id,'last_name')]")).clear();
+		driver.findElement(By.xpath("//input[contains(@id,'last_name')]")).sendKeys("Testlastname");
 		// Enter Public Display Name
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[3]/input")).clear();
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[3]/input"))
-				.sendKeys("Test");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@id,'display_name')]")));
+		driver.findElement(By.xpath("//input[contains(@id,'display_name')]")).clear();
+		driver.findElement(By.xpath("//input[contains(@id,'display_name')]")).sendKeys("Testdisplayname");
 		// Enter About Me
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/fieldset[4]/textarea"))
-				.sendKeys("This is About me");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[contains(@id,'description')]")));
+		driver.findElement(By.xpath("//textarea[contains(@id,'description')]")).clear();
+		driver.findElement(By.xpath("//textarea[contains(@id,'description')]")).sendKeys("Testdisplayname");
 		// Click on 'Toggle' button
-		driver.findElement(By.id("inspector-toggle-control-0")).click();
+		driver.findElement(By.xpath("//input[contains(@id,'inspector-toggle-control-0')]")).click();
 		JavascriptExecutor js1 = (JavascriptExecutor) driver;
 		js1.executeScript("window.scrollBy(0,200)");
 		// Click on 'Save Profile details' Button
 		// driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/form/p/button")).click();
 		// Upload Profile Picture
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/div/div[1]/span/div/div[2]/span"))
+		driver.findElement(
+				By.xpath("//div[@class='edit-gravatar__label-container']//span[@class='edit-gravatar__label']"))
 				.click();
 		Robot robot = new Robot();
 		robot.setAutoDelay(4000);
@@ -85,9 +83,10 @@ public class My_Profile_Test extends TestBase {
 		robot.keyPress(KeyEvent.VK_V);
 		robot.setAutoDelay(1000);
 		robot.keyPress(KeyEvent.VK_ENTER);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div/div/div/div/figure/div/div[1]/div/div[5]")));
 		driver.findElement(By.xpath("/html/body/div[4]/div/div/div/div/figure/div/div[3]/button[3]")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		String FileUploadSuccessMessage = driver
 				.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/span[2]/span")).getText();
 		System.out.println(FileUploadSuccessMessage);
@@ -105,4 +104,3 @@ public class My_Profile_Test extends TestBase {
 
 	}
 
-}
